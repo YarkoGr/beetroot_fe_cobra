2b3c30c97e36af8073de1e6302e3f8653f45aa44
Yaroslav Pasko <yaroslavpasko81@gmail.com>
2020-10-21 15:02:56 +0300

    added button up

--- projects/task-cahee-resp-top-nav/src/js/app.js
+++ projects/task-cahee-resp-top-nav/src/js/app.js
@@ -1,10 +1,36 @@
-$('.navicon').on('click', function(e) {
+$(".navicon").on("click", function(e) {
     e.preventDefault();
-    $(this).toggleClass('navicon--active');
-    $('.toggle').toggleClass('toggle--active');
+    $(this).toggleClass("navicon--active");
+    $(".toggle").toggleClass("toggle--active");
 });
-
-
 
 AOS.init();
 
+//scroll up start
+
+let scrollToTopBtn = document.querySelector(".scrollToTopBtn");
+let rootElement = document.documentElement;
+
+function handleScroll() {
+    // Do something on scroll
+    let scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+    if (rootElement.scrollTop / scrollTotal > 0.1) {
+        // Show button
+        scrollToTopBtn.classList.add("showBtn");
+    } else {
+        // Hide button
+        scrollToTopBtn.classList.remove("showBtn");
+    }
+}
+
+function scrollToTop() {
+    // Scroll to top logic
+    rootElement.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
+document.addEventListener("scroll", handleScroll);
+
+//scroll up end
